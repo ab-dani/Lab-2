a0a91fa47e1864207893c28624f37e80ffa67978
Antonio Brown <135006436+ab-dani@users.noreply.github.com>
2023-07-08 18:18:34 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let color = ['What is your favorite color?']        
+
+console.log('My favorite color is, ' + color);
+
+let shoe = ['What is your favorite shoe?']
+
+console.log('My favorite shoe is, ' + shoe);
+
+let food = ['What is your favorite food?']
+
+console.log('My favorite food is, ' + food);
+
+let trav = ['Do you like to travel?']
+
+console.log('My favorite place to travel' + trav)
